686dee9f3965b666eb83f2eebb745770099ad3cf
chunpaiyang <15076105+chunpaiyang@users.noreply.github.com>
2018-08-30 13:48:52 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,25 @@ var gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const fs = require('fs');
 const { spawnSync } = require('child_process');
+var forever = require("forever-monitor");
+var bg = require("gulp-bg");
 
 gulp.task('setup', () => {
     spawnSync('./setup/setup.sh');
 });
 
-gulp.task('localstack', () => {
-    spawnSync('./setup/start.sh');
-});
+gulp.task('localstack',bg("./setup/start.sh"));
+
+const exitCallback = (proc) => { if (proc.errorcode != 0) { process.exit(proc.errorcode); } };
+gulp.task("stop", () => {
+    bgtask.setCallback(exitCallback);
+    bgtask.stop();
+  }
+);
 
 gulp.task('test', () => {
     gulp.src('test/unittest.js', {read: false})
         .pipe(mocha({'reporter': 'nyan'}));
 });
 
-gulp.task('default', ['test']);
+gulp.task('default', ['localstack']);
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+WORKD=`[ -z "$1" ] && echo "../" || "$1"`
+echo $WORKD
+
+cd $WORKD
+
 NODE_VER=8.10.0
 NODE_ENV="env/node8.10_env"
 PYTHON_BIN=/usr/bin/python2.7
@@ -7,28 +12,14 @@ PYTHON_ENV="env/python2.7_env"
 
 mkdir -p env
 
-pip install nodeenv || \
-    (echo "install nodeenv failed" && exit 1);
-nodeenv ${NODE_ENV} --node=${NODE_VER} || \
-    (echo "create node vritualenv failed" && exit 1);
-virtualenv ${PYTHON_ENV} --python=${PYTHON_BIN} || \
-    (echo "create python2.7 virtualenv failed"  && exit 1);
-
-source ${NODE_ENV}/bin/activate || \
-    (echo "failed to activate node virtual env" && exit 1);
-npm install --save-dev  || \
-    (echo "install package.json devDependencies failed" && exit 1);
-npm install --save  || \
-    (echo "install package.json Dependencies failed" && exit 1);
-deactivate_node || \
-    (echo "failed to deactivate_node" && exit 1);
-
-source ${PYTHON_ENV}/bin/activate || \
-    (echo "failed ot activate python27 virtual env" && exit 1);
-
-pip install localstack || \
-    (echo "failed to install localstack" && exit 1);
-deactivate || \
-    (echo "failed to deactivate" && exit 1);
-
+pip install nodeenv && \
+nodeenv ${NODE_ENV} --node=${NODE_VER} && \
+virtualenv ${PYTHON_ENV} --python=${PYTHON_BIN} && \
+. ${NODE_ENV}/bin/activate && \
+npm install --save-dev  && \
+npm install --save  && \
+deactivate_node && \
+. ${PYTHON_ENV}/bin/activate && \
+pip install localstack && \
+deactivate && \
 exit 0;
